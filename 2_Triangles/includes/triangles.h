dd87dc4d449940ffe44319092dc818610b028f0c
realFrogboy <avdeev.do@phystech.edu>
2022-09-15 02:14:58 +0500

    Add plates, triangles, operators for vector

--- 2_Triangles/includes/triangles.h
+++ 2_Triangles/includes/triangles.h
@@ -10,12 +10,6 @@ struct coord_t {
     double z;
 };
 
-struct coeffs_t {
-    double a;
-    double b;
-    double c;
-};
-
 
 class vector_t {
     coord_t coord{NAN, NAN, NAN};
@@ -24,6 +18,7 @@ class vector_t {
     vector_t(double x, double y, double z) : coord{x, y, z} {};
 
     coord_t getCoord() const { return coord; };
+
     void copy(const vector_t& vec) {
         coord.x = vec.coord.x;
         coord.y = vec.coord.y;
@@ -42,11 +37,20 @@ class vector_t {
         return 0;
     }
 
-    double scalarMult (const vector_t& vec) const { 
+    vector_t constMult(const double mul) const {
+        double x = coord.x * mul;
+        double y = coord.y * mul;
+        double z = coord.z * mul;
+
+        vector_t res(x, y, z);
+        return res;
+    }
+
+    double scalarMult(const vector_t& vec) const { 
         return (coord.x * vec.coord.x + coord.y * vec.coord.y + coord.z * vec.coord.z); 
     }
 
-    vector_t vectorMult (const vector_t& vec) const {
+    vector_t vectorMult(const vector_t& vec) const {
         double x = coord.y * vec.coord.z - coord.z * vec.coord.y;
         double y = coord.z * vec.coord.x - coord.x * vec.coord.z;
         double z = coord.x * vec.coord.y - coord.y * vec.coord.x;
@@ -72,6 +76,11 @@ class vector_t {
         vector_t res(x, y, z);
         return res;
     }
+
+    double length() {
+       double tmp =  this->scalarMult(*this);
+       return sqrt(tmp);
+    }
 };
 
 class point_t {
@@ -79,6 +88,7 @@ class point_t {
 
     public:
     point_t(double x, double y, double z) : coord{x, y, z} {};
+
     coord_t getCoord() const { return coord; }
 
     void copy(const point_t& pt) {
@@ -102,59 +112,57 @@ class point_t {
     vector_t toVector() const {
         vector_t vec(coord.x, coord.y, coord.z);
         return vec;
-    } 
+    }
 };
 
-/*class line_t { //TODO: 3d
-    coeffs_t coeffs{NAN, NAN, NAN};
+
+struct lineData_t {
+    vector_t a;
+    point_t linePt;
+};
+
+class line_t {
+    lineData_t lineData{{NAN, NAN, NAN}, {NAN, NAN, NAN}};
 
     public:
-    line_t(double a, double b, double c) : coeffs{a, b, c} {};
-    line_t(const point_t& firstPt, const point_t& secondPt) {
-        coord_t firstCoord  = firstPt.getCoord();
-        coord_t secondCoord = secondPt.getCoord();
+    line_t(vector_t dir, point_t pt) : lineData{{NAN, NAN, NAN}, {NAN, NAN, NAN}} {
+        lineData.a.copy(dir);
+        lineData.linePt.copy(pt);
+    }
 
-        double a = (firstCoord.y - secondCoord.y) / (secondCoord.x - firstCoord.x);
-        double c = -a * firstCoord.x - firstCoord.y;
+    line_t(const point_t& firstPt, const point_t& secondPt) : lineData{{NAN, NAN, NAN}, {NAN, NAN, NAN}} {
+        vector_t firstVec  = firstPt.toVector();
+        vector_t secondVec = secondPt.toVector();
 
-        coeffs.a = a;
-        coeffs.b = 1.0;
-        coeffs.c = c;
-    };
+        lineData.a.copy(secondVec - firstVec);
+        lineData.linePt.copy(firstPt);
+    }
 
-    coeffs_t getCoeffs() const { return coeffs; }
+    lineData_t getData() const { return lineData; }
 
     bool valid() const {
-        if ((isfinite(coeffs.a)) && (isfinite(coeffs.b)) && (isfinite(coeffs.c)))
+        if ((lineData.a.valid()) && (lineData.linePt.valid()))
             return 1;
         return 0;
-    };
+    }
 
-    point_t isCross(const line_t& line) const {
-        double kA = coeffs.a / line.coeffs.a;
-        double kB = coeffs.b / line.coeffs.b;
+    vector_t normal() const {
 
-        // parallel or equal lines
-        if (abs(kA - kB) <= accurasy) {
-            point_t pt(NAN, NAN);
-            return pt;
-        }
+    }
+};
 
-        // not parallel or equal lines
-        double x0 = (coeffs.b * line.coeffs.c - line.coeffs.b * coeffs.c) / (coeffs.a * line.coeffs.b - line.coeffs.a * coeffs.b);
-        double y0 = (-coeffs.a * x0 - coeffs.c) / coeffs.b;
-        point_t pt(x0, y0);
-        return pt;
-    };
-};*/
+
+struct plateData_t {
+    vector_t n;
+    point_t platePt;
+    double d;
+};
 
 class plate_t {
-    vector_t n;
-    point_t pt;
-    double d;
+    plateData_t pltData {{NAN, NAN, NAN}, {NAN, NAN, NAN}, NAN};
 
     public:
-    plate_t(const point_t& firstPt, const point_t& secondPt, const point_t& therdPt) : n{NAN, NAN, NAN}, pt{NAN, NAN, NAN}{
+    plate_t(const point_t& firstPt, const point_t& secondPt, const point_t& therdPt) : pltData {{NAN, NAN, NAN}, {NAN, NAN, NAN}, NAN} {
         vector_t firstVec  = firstPt.toVector();
         vector_t secondVec = secondPt.toVector();
         vector_t therdVec  = therdPt.toVector();
@@ -167,8 +175,110 @@ class plate_t {
         tmpCoord.z /= normalCoeff;
 
         vector_t norm(tmpCoord.x, tmpCoord.y, tmpCoord.z);
-        n.copy(norm);
-        pt.copy(firstPt);
-        d = -firstVec.scalarMult(n);
+
+        pltData.n.copy(norm);
+        pltData.platePt.copy(firstPt);
+        pltData.d = -firstVec.scalarMult(pltData.n);
+    }
+
+    plateData_t getData() const { return pltData; }
+
+    bool valid() {
+        if ((pltData.n.valid()) && (pltData.platePt.valid()) && (isfinite(pltData.d)))
+            return 1;
+        return 0;
+    }
+
+    bool isParallel(const plate_t& plt) const {
+        if ((pltData.n.isEqual(plt.pltData.n)) && (pltData.platePt.isEqual(plt.pltData.platePt)))
+            return 1;
+        return 0;
+    }
+
+    bool isEqual(const plate_t& plt) const {
+        if ((pltData.n.isEqual(plt.pltData.n)) && (pltData.platePt.isEqual(plt.pltData.platePt)) && (abs(pltData.d - plt.pltData.d) <= accurasy))
+            return 1;
+        return 0;
+    }
+
+    double distToPoint(const point_t& pt) const {
+        vector_t ptVec = pt.toVector();
+        vector_t plateVec = pltData.platePt.toVector();
+
+        vector_t vec = ptVec - plateVec;
+
+        double dist = abs(pltData.n.scalarMult(vec));
+        double r = pltData.n.scalarMult(ptVec) + dist;
+
+        return r;
+    }
+
+    point_t projOfPt(const point_t& pt) const {
+        plateData_t pltData = this->getData();
+        vector_t thisVec = pt.toVector();
+
+        vector_t projVec = thisVec - pltData.n.constMult((thisVec.scalarMult(pltData.n) + pltData.d));
+        coord_t vecCoord = projVec.getCoord();
+
+        point_t pt(vecCoord.x, vecCoord.y, vecCoord.z);
+        return pt;
+    }
+
+    line_t intsectOf2Plt(const plate_t& plt) const {
+        // parallel or equal plates
+        if(this->isParallel(plt)) {
+            vector_t a{NAN, NAN, NAN};
+            point_t pt{NAN, NAN, NAN};
+            line_t res(a, pt);
+            return res;
+        }
+
+        // not parallel or equal plates
+        vector_t aVec = pltData.n.vectorMult(plt.pltData.n);
+        double s1 = -pltData.d;
+        double s2 = -plt.pltData.d;
+
+        double a = (s2 * pltData.n.scalarMult(plt.pltData.n) - s1 * plt.pltData.n.scalarMult(plt.pltData.n)) / 
+                    (pltData.n.scalarMult(plt.pltData.n) * pltData.n.scalarMult(plt.pltData.n) - pltData.n.scalarMult(pltData.n) * plt.pltData.n.scalarMult(plt.pltData.n));
+        double b = (s1 * pltData.n.scalarMult(plt.pltData.n) - s2 * pltData.n.scalarMult(pltData.n)) / 
+                    (pltData.n.scalarMult(plt.pltData.n) * pltData.n.scalarMult(plt.pltData.n) - pltData.n.scalarMult(pltData.n) * plt.pltData.n.scalarMult(plt.pltData.n));
+
+        vector_t commVec = pltData.n.constMult(a) + plt.pltData.n.constMult(b);
+        coord_t coords = commVec.getCoord();
+        point_t commPt(coords.x, coords.y, coords.z);
+
+        line_t line(aVec, commPt);
+        return line;
+    }
+};
+
+
+struct trianglePt_t {
+    point_t first;
+    point_t second;
+    point_t third;
+};
+
+class triangle_t {
+    trianglePt_t trianglePt{{NAN, NAN, NAN}, {NAN, NAN, NAN}, {NAN, NAN, NAN}};
+
+    public:
+    triangle_t(point_t pt1, point_t pt2, point_t pt3) : trianglePt{{NAN, NAN, NAN}, {NAN, NAN, NAN}, {NAN, NAN, NAN}} {
+        trianglePt.first.copy(pt1);
+        trianglePt.second.copy(pt2);
+        trianglePt.third.copy(pt3);
+    }
+
+    triangle_t projOntoPlt(const plate_t& plt) {
+        point_t projFirst  = plt.projOfPt(trianglePt.first);
+        point_t projSecond = plt.projOfPt(trianglePt.second);
+        point_t projThird  = plt.projOfPt(trianglePt.third);
+
+        triangle_t triangle(projFirst, projSecond, projThird);
+        return triangle;
+    }
+
+    bool intersection2D(const triangle_t& triang) const {
+
     }
 };
