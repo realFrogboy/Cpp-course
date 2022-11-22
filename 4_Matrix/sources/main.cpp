86e4aa41c34c1052c5d3c8f61a93413a81f0094c
realFrogboy <avdeev.do@phystech.edu>
2022-11-23 01:14:35 +0500

    Insignificant changes

--- 4_Matrix/sources/main.cpp
+++ 4_Matrix/sources/main.cpp
@@ -4,10 +4,11 @@
 #include <vector>
 
 int main() {
-    std::pair<unsigned, std::vector<double>> pr = get_data::get_data();
     double res = 0;
 
     try {
+        std::pair<unsigned, std::vector<double>> pr = get_data::get_data();
+        
         matrix::matrix_t matrix(pr.second, pr.first);
         res = matrix.determinant();
     } catch (std::exception &e) {
